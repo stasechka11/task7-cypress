aaf929b82d35be51e403e8d6c4a4719184decf5c
Anastasiia Fokina <stasechka11@yandex.ru>
2024-07-14 16:07:35 +0300

    selectors for main page

--- cypress/e2e/cinema_main_page.cy.js
+++ cypress/e2e/cinema_main_page.cy.js
@@ -1,11 +1,13 @@
+const selector = require("../fixtures/selectors-main-page.json")
+
 describe('Cinema main page tests', () => {
   beforeEach( () => {
     cy.visit('/');
   })
 
   it('Check number of days', () => {
-    cy.get('.page-header__title').should("have.text", "Идем в кино");
-    cy.get('.page-nav__day').should("have.length", 7);
+    cy.get(selector.headerTitle).should("have.text", "Идем в кино");
+    cy.get(selector.days).should("have.length", 7);
   })
-  
+
 })
